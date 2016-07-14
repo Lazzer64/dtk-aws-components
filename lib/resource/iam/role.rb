3f5b4bc8c415b79f879392962156d2c3d94b7026
Alex Pelavin <ajpele@gmail.com>
2016-07-14 12:06:35 -0700

    Separated assume role policy document into separate properties

--- lib/resource/iam/role.rb
+++ lib/resource/iam/role.rb
@@ -3,25 +3,41 @@ class Resource
     class Role < self
       METADATA = {
         role_name: [:key, :create],
-        assume_role_policy_document: [:create],
+        Effect: [:create],
+        Principal: [:create],
+        Action: [:create],
         policies: [:update_policy]
       }.freeze
 
       private 
 
+      def build_role_policy_document
+        @desired_properties[:assume_role_policy_document] = {
+          "Version" => "2012-10-17",
+          "Statement" => [
+            {
+              'Effect' => @desired_properties[:Effect],
+              'Principal' => @desired_properties[:Principal],
+              'Action' => @desired_properties[:Action]
+            }
+          ]
+        }.to_json
+      end
+
       def create_resource
+        build_role_policy_document
         @aws_client.create_role(role_name: @desired_properties[:role_name], assume_role_policy_document: @desired_properties[:assume_role_policy_document])
       end
 
       def delete_resource
-        detach_polocies
+        detach_policies
         @aws_client.delete_role(role_name: @desired_properties[:role_name])
       end
 
       def process_diff(diff)
         diff.each do |key, val|
           if keys(:update_policy).include?(key)
-            detach_polocies
+            detach_policies
             val.each do |policy_arn|
               @aws_client.attach_role_policy(role_name: @desired_properties[:role_name], policy_arn: policy_arn)
             end
@@ -38,7 +54,7 @@ class Resource
         nil
       end
 
-      def detach_polocies
+      def detach_policies
         policies = @aws_client.list_attached_role_policies(role_name: @desired_properties[:role_name]).attached_policies
         policies.each do |policy|
           @aws_client.detach_role_policy(role_name: @desired_properties[:role_name], policy_arn: policy.policy_arn)
